c853faa8e87d5577ed5b861d35d6c4d580706879
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-11-10 06:39:13 +0600

    Fixed Minor

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (res, req) => {
+app.get('/', (req, res) => {
     res.send('Server Running Happily...');
 });
 
