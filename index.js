8067deae133546f5856147347e05b1290f685fa4
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-11-12 23:08:42 +0600

    Connected to MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ObjectId } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}.gmeoo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+// func mongo
+async function run() {
+    try {
+      await client.connect();
+
+      const database = client.db(process.env.DB_NAME);
+
+      const userCollection = database.collection(process.env.DB_COLLECTIONS_USER);
+      const productCollection = database.collection(process.env.DB_COLLECTIONS_PRODUCT);
+      const orderCollection = database.collection(process.env.DB_COLLECTIONS_ORDER);
+      const reviewCollection = database.collection(process.env.DB_COLLECTIONS_REVIEW);
+
+      const doc = {
+        title: "Record of a Shriveled Datum",
+        content: "No bytes, no problem. Just insert a document, in MongoDB",
+      }
+      const result = await userCollection.insertOne(doc);
+
+    } finally {
+    //   Ensures that the client will close when you finish/error
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Server Running Happily...');
